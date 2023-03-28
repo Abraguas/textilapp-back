c672afcfd8808347572767344c22aaff9cb21ffc
Abraguas <fmoises2002@gmail.com>
2023-03-28 11:08:05 -0300

    Initial Commit

---
+++ src/main/java/com/tup/textilapp/TextilappApplication.java
@@ -0,0 +1,13 @@
+package com.tup.textilapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextilappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextilappApplication.class, args);
+	}
+
+}

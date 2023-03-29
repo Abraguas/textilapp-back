d13a02d25fc0b8e04baea409f1b55c316f1e7300
Abraguas <fmoises2002@gmail.com>
2023-03-29 14:52:32 -0300

    Modify RoleConfig to be scalable

--- src/main/java/com/tup/textilapp/config/RoleConfig.java
+++ src/main/java/com/tup/textilapp/config/RoleConfig.java
@@ -5,19 +5,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.tup.textilapp.model.entity.Role;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Configuration
-public class RoleConfig {
-    @Bean
-    CommandLineRunner commandLineRunner(RoleRepository repository){
-        return args -> {
-            List<Role> list = new ArrayList<>();
-            list.add(new Role(null,"ADMIN"));
-            list.add(new Role(null,"CLIENT"));
-            repository.saveAll(list);
-        };
+@Component
+public class RoleConfig implements CommandLineRunner {
+
+    private final RoleRepository repository;
+
+    public RoleConfig(RoleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<Role> list = new ArrayList<>();
+        list.add(new Role(null, "ADMIN"));
+        list.add(new Role(null, "CLIENT"));
+        repository.saveAll(list);
     }
 }

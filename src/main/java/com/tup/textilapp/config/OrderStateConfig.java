4137989691f613ae86b16d047ed3cf63cded383c
Abraguas <fmoises2002@gmail.com>
2023-04-04 12:38:43 -0300

    Create OrderState data initializer and repo

---
+++ src/main/java/com/tup/textilapp/config/OrderStateConfig.java
@@ -0,0 +1,37 @@
+package com.tup.textilapp.config;
+
+
+import com.tup.textilapp.model.entity.OrderState;
+import com.tup.textilapp.repository.OrderStateRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class OrderStateConfig implements CommandLineRunner {
+
+    private final OrderStateRepository repository;
+    @Value("${data.initialized}")
+    private boolean dataInitialized;
+    public OrderStateConfig(OrderStateRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (dataInitialized) {
+            return;
+        }
+
+        List<OrderState> list = new ArrayList<>();
+        list.add(new OrderState(null, "Pendiente"));
+        list.add(new OrderState(null, "Aprobado"));
+        list.add(new OrderState(null, "Cobrado"));
+        list.add(new OrderState(null, "Entregado"));
+        repository.saveAll(list);
+
+    }
+}

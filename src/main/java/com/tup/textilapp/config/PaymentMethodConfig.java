cf093528805560619defc72307ca7cd785d5c415
Abraguas <fmoises2002@gmail.com>
2023-04-03 22:59:37 -0300

    Create PaymentMethodConfig and PaymentMethodRepository

---
+++ src/main/java/com/tup/textilapp/config/PaymentMethodConfig.java
@@ -0,0 +1,36 @@
+package com.tup.textilapp.config;
+
+
+import com.tup.textilapp.model.entity.PaymentMethod;
+import com.tup.textilapp.repository.PaymentMethodRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PaymentMethodConfig implements CommandLineRunner {
+
+    private final PaymentMethodRepository repository;
+    @Value("${data.initialized}")
+    private boolean dataInitialized;
+    public PaymentMethodConfig(PaymentMethodRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... args) {
+        if (dataInitialized) {
+            return;
+        }
+
+        List<PaymentMethod> list = new ArrayList<>();
+        list.add(new PaymentMethod(null, "Mercado Pago"));
+        list.add(new PaymentMethod(null, "Presencial"));
+        repository.saveAll(list);
+
+    }
+}
+
---
+++ src/main/java/com/tup/textilapp/repository/PaymentMethodRepository.java
@@ -0,0 +1,10 @@
+package com.tup.textilapp.repository;
+
+import com.tup.textilapp.model.entity.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Integer> {
+
+}

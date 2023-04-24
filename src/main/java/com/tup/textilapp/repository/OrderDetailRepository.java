8156ddfbe1564f35a41597de7cd3115767248535
Abraguas <fmoises2002@gmail.com>
2023-04-24 13:24:53 -0300

    Add findAllByProduct method

--- src/main/java/com/tup/textilapp/repository/OrderDetailRepository.java
+++ src/main/java/com/tup/textilapp/repository/OrderDetailRepository.java
@@ -1,10 +1,13 @@
 package com.tup.textilapp.repository;
 
 import com.tup.textilapp.model.entity.OrderDetail;
+import com.tup.textilapp.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Integer> {
-
+    List<OrderDetail> findAllByProduct(Product product);
 }

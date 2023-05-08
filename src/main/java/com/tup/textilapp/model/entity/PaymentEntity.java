2a32a38301e7e064529b1f2e94488cb9b30351ea
Abraguas <fmoises2002@gmail.com>
2023-05-08 10:33:37 -0300

    Rename PaymentEntity, replace bill with order field and change tnumber type

---
+++ src/main/java/com/tup/textilapp/model/entity/PaymentEntity.java
@@ -0,0 +1,30 @@
+package com.tup.textilapp.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payments")
+public class PaymentEntity {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Date date;
+    private String observations;
+    private BigDecimal ammountCharged;
+    private String transactionNumber;
+    @ManyToOne(targetEntity = Order.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id", referencedColumnName = "id")
+    private Order order;
+    @ManyToOne(targetEntity = PaymentMethod.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "payment_method_id", referencedColumnName = "id")
+    private PaymentMethod paymentMethod;
+}

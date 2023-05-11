0272a0aae1e404a61cad8e739356bd866b50c935
Abraguas <fmoises2002@gmail.com>
2023-05-11 13:01:31 -0300

    Create payment method controller and getAll()

--- src/main/java/com/tup/textilapp/controller/PaymentMethodController.java
+++ src/main/java/com/tup/textilapp/controller/PaymentMethodController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "payment-method")
+@RequestMapping("/payment-method")
 public class PaymentMethodController {
     private final PaymentMethodService paymentMethodService;
     @Autowired
